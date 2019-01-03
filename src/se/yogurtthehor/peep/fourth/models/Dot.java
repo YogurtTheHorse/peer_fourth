5466038ff6b3035bba7d8b2f8ede43278cd368d2
yegorf1 <yegorf1@gmail.com>
2019-01-03 22:09:59 +0300

    Add dot model

---
+++ src/se/yogurtthehor/peep/fourth/models/Dot.java
@@ -0,0 +1,60 @@
+package se.yogurtthehor.peep.fourth.models;
+
+import java.io.Serializable;
+
+public class Dot implements Serializable {
+    private int id;
+    private double x;
+    private double y;
+    private double r;
+    private String author;
+    private boolean checked;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getR() {
+        return r;
+    }
+
+    public void setR(double r) {
+        this.r = r;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+}
