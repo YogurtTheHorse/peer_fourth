d2d7d4bcd30ac45f24a5a7cc88c86b0c056d631e
yegorf1 <yegorf1@gmail.com>
2019-01-03 22:10:39 +0300

    Add login request and result classes

---
+++ src/se/yogurtthehor/peep/fourth/entities/LoginResult.java
@@ -0,0 +1,25 @@
+package se.yogurtthehor.peep.fourth.entities;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class LoginResult {
+    private boolean success;
+    private String message;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
